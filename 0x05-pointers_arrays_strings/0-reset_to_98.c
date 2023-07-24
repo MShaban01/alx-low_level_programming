b91d67acf7bbcda601696741bd7f73e10f6bea05
MShaban01 <abubakarrmalikie@gmail.com>
2023-07-24 07:25:25 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,11 +2,11 @@
 
 /**
  * reset_to_98 - reset to 98
- * @n: pointer to the value to be changed
- * Return: void
+ * @n: input
+ * Return: n
  */
 
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
