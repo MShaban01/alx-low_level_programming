e5043cbe16b29205abcd512ba4027c7bec0c60e7
MShaban01 <abubakarrmalikie@gmail.com>
2023-07-26 08:00:53 -0700

    5-string_toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,24 +1,20 @@
 #include "main.h"
-
 /**
- * rev_string - Reverses a string
- * @s: Input string
- * Return: String in reverse
+ * string_toupper - change all lowercase to uppercase
+ * @n: pointer
+ *
+ * Return: n
  */
-
-void rev_string(char *s)
+char *string_toupper(char *n)
 {
-	char rev = s[0];
-	int counter = 0;
 	int i;
 
-	while (s[counter] != '\0')
-		counter++;
-	for (i = 0; i < counter; i++)
+	i = 0;
+	while (n[i] != '\0')
 	{
-		counter--;
-		rev = s[i];
-		s[i] = s[counter];
-		s[counter] = rev;
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
 	}
+	return (n);
 }
