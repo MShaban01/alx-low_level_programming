e48912d44abe1122bd60465c8a450affc3e24d96
MShaban01 <abubakarrmalikie@gmail.com>
2023-07-26 19:22:23 -0700

    3-strcmp

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,23 +1,30 @@
 #include "main.h"
-
 /**
- * _strncat - concatenates two strings
- * @dest: destination string
- * @src: source string
- * @n: maximum number of characters to append
+ * _strncat - concatenate two strings
+ * using at most n bytes from src
+ * @dest: input value
+ * @src: input value
+ * @n: input value
  *
- * Return: pointer to the resulting string
+ * Return: dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int len = strlen(dest);
 	int i;
+	int j;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	i = 0;
+	while (dest[i] != '\0')
 	{
-		dest[len + i] = src[i];
+		i++;
 	}
-	dest[len + i] = '\0';
-
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
